72510a6d850c0f305be92ec7b58f81ac09f4f140
Khac Duy <2002nguyenkhacduy@gmail.com>
2023-08-25 17:02:58 +0700

    add other entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,30 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
 import { UserModule } from './user/user.module';
 import { CurrentUserMiddleware } from './utilities/middlewares/current-user.middlewares';
+import { MovieModule } from './movie/movie.module';
+import { HallModule } from './hall/hall.module';
+import { ShowtimeModule } from './showtime/showtime.module';
+import { BookingModule } from './booking/booking.module';
+import { RatingModule } from './rating/rating.module';
+import { BillModule } from './bill/bill.module';
+import { GenreModule } from './genre/genre.module';
+import { DirectorModule } from './director/director.module';
+import { CastModule } from './cast/cast.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    UserModule,
+    MovieModule,
+    HallModule,
+    ShowtimeModule,
+    BookingModule,
+    RatingModule,
+    BillModule,
+    GenreModule,
+    DirectorModule,
+    CastModule,
+  ],
   controllers: [],
   providers: [],
 })
