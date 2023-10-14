f4e8ee224b3c9586a5ee270b72ba72b8421956ee
NgKhacDuy <2002nguyenkhacduy@gmail.com>
2023-10-14 11:17:56 +0700

    add full table

--- src/user/dto/update-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class UpdateUserDto {
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
-  @IsNotEmpty()
-  name: string;
-}
--- src/user/dto/user-changePass.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class UserChangePassDto {
-  @IsNotEmpty()
-  password: string;
-}
