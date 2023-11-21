37e639022b02e758d6a505d8f09260075f5ff412
NgKhacDuy <2002nguyenkhacduy@gmail.com>
2023-11-21 18:11:05 +0700

    update dto updateUser

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,15 +1,16 @@
+import { PartialType } from '@nestjs/mapped-types';
 import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { GENDER } from 'src/utilities/common/user-gender.enum';
 
 export class UpdateUserDto {
-  @IsNotEmpty()
   @IsEmail()
   email: string;
   @IsString()
-  name: string;
+  firstName: string;
+  @IsString()
+  lastName: string;
   @IsString()
   phoneNumber: string;
-  @IsNotEmpty()
   @IsEnum(GENDER)
   gender: GENDER;
 }
