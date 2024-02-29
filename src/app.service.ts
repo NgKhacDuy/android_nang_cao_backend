fc5f49bdcbaaeebdb3f6ab096bad6cb7f934a640
NgKhacDuy <2002nguyenkhacduy@gmail.com>
2024-02-29 10:39:46 +0700

    add cronjon

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  // @Cron(CronExpression.EVERY_10_MINUTES)
-  // refreshServer() {
-  //   console.log('refresh server');
-  // }
+  @Cron(CronExpression.EVERY_10_MINUTES)
+  async refreshServer() {
+    const request = await axios.get(
+      'https://android-nang-cao-backend.onrender.com/api/v1/status',
+    );
+    if (request.status !== 200) {
+      console.log('Cron job failed');
+    } else {
+      console.log('Cron job successfully');
+    }
+  }
 }
